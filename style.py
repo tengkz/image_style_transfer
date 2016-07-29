3e0930b95a9ab98c8a5b31adc071d492e1949822
滕可振 <hztengkezhen@corp.netease.com>
2016-07-29 17:29:33 +0800

    add style.py

--- style.py
+++ style.py
@@ -36,48 +36,41 @@ from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
 from lasagne.layers import Pool2DLayer as PoolLayer
 from lasagne.nonlinearities import softmax
 
-IMAGE_W = 600
+IMAGE_W = 128
+
+loss_alpha1 = 0.001
+loss_alpha2 = 0.2
+loss_alpha3 = 0.1e-7
+
 #DST='1920px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'
 #SRC='Tuebingen_Neckarfront.jpg'
-
 SRC = 'src.jpg'
-DST = 'dst.jpg'
-
-length_1 = 64
-length_2 = 128
-length_3 = 256
-length_4 = 512
-length_5 = 512
-#length_1 = 32
-#length_2 = 64
-#length_3 = 128
-#length_4 = 256
-#length_5 = 256
+DST = 'wen2.jpg'
 
 # Note: tweaked to use average pooling instead of maxpooling
 def build_model():
     net = {}
     net['input'] = InputLayer((1, 3, IMAGE_W, IMAGE_W))
-    net['conv1_1'] = ConvLayer(net['input'], length_1, 3, pad=1, flip_filters=False)
-    net['conv1_2'] = ConvLayer(net['conv1_1'], length_1, 3, pad=1, flip_filters=False)
+    net['conv1_1'] = ConvLayer(net['input'], 64, 3, pad=1, flip_filters=False)
+    net['conv1_2'] = ConvLayer(net['conv1_1'], 64, 3, pad=1, flip_filters=False)
     net['pool1'] = PoolLayer(net['conv1_2'], 2, mode='average_exc_pad')
-    net['conv2_1'] = ConvLayer(net['pool1'], length_2, 3, pad=1, flip_filters=False)
-    net['conv2_2'] = ConvLayer(net['conv2_1'], length_2, 3, pad=1, flip_filters=False)
+    net['conv2_1'] = ConvLayer(net['pool1'], 128, 3, pad=1, flip_filters=False)
+    net['conv2_2'] = ConvLayer(net['conv2_1'], 128, 3, pad=1, flip_filters=False)
     net['pool2'] = PoolLayer(net['conv2_2'], 2, mode='average_exc_pad')
-    net['conv3_1'] = ConvLayer(net['pool2'], length_3, 3, pad=1, flip_filters=False)
-    net['conv3_2'] = ConvLayer(net['conv3_1'], length_3, 3, pad=1, flip_filters=False)
-    net['conv3_3'] = ConvLayer(net['conv3_2'], length_3, 3, pad=1, flip_filters=False)
-    net['conv3_4'] = ConvLayer(net['conv3_3'], length_3, 3, pad=1, flip_filters=False)
+    net['conv3_1'] = ConvLayer(net['pool2'], 256, 3, pad=1, flip_filters=False)
+    net['conv3_2'] = ConvLayer(net['conv3_1'], 256, 3, pad=1, flip_filters=False)
+    net['conv3_3'] = ConvLayer(net['conv3_2'], 256, 3, pad=1, flip_filters=False)
+    net['conv3_4'] = ConvLayer(net['conv3_3'], 256, 3, pad=1, flip_filters=False)
     net['pool3'] = PoolLayer(net['conv3_4'], 2, mode='average_exc_pad')
-    net['conv4_1'] = ConvLayer(net['pool3'], length_4, 3, pad=1, flip_filters=False)
-    net['conv4_2'] = ConvLayer(net['conv4_1'], length_4, 3, pad=1, flip_filters=False)
-    net['conv4_3'] = ConvLayer(net['conv4_2'], length_4, 3, pad=1, flip_filters=False)
-    net['conv4_4'] = ConvLayer(net['conv4_3'], length_4, 3, pad=1, flip_filters=False)
+    net['conv4_1'] = ConvLayer(net['pool3'], 512, 3, pad=1, flip_filters=False)
+    net['conv4_2'] = ConvLayer(net['conv4_1'], 512, 3, pad=1, flip_filters=False)
+    net['conv4_3'] = ConvLayer(net['conv4_2'], 512, 3, pad=1, flip_filters=False)
+    net['conv4_4'] = ConvLayer(net['conv4_3'], 512, 3, pad=1, flip_filters=False)
     net['pool4'] = PoolLayer(net['conv4_4'], 2, mode='average_exc_pad')
-    net['conv5_1'] = ConvLayer(net['pool4'], length_5, 3, pad=1, flip_filters=False)
-    net['conv5_2'] = ConvLayer(net['conv5_1'], length_5, 3, pad=1, flip_filters=False)
-    net['conv5_3'] = ConvLayer(net['conv5_2'], length_5, 3, pad=1, flip_filters=False)
-    net['conv5_4'] = ConvLayer(net['conv5_3'], length_5, 3, pad=1, flip_filters=False)
+    net['conv5_1'] = ConvLayer(net['pool4'], 512, 3, pad=1, flip_filters=False)
+    net['conv5_2'] = ConvLayer(net['conv5_1'], 512, 3, pad=1, flip_filters=False)
+    net['conv5_3'] = ConvLayer(net['conv5_2'], 512, 3, pad=1, flip_filters=False)
+    net['conv5_4'] = ConvLayer(net['conv5_3'], 512, 3, pad=1, flip_filters=False)
     net['pool5'] = PoolLayer(net['conv5_4'], 2, mode='average_exc_pad')
 
     return net
@@ -179,6 +172,7 @@ def total_variation_loss(x):
 # In[9]:
 
 layers = ['conv4_2', 'conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1']
+#layers = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1']
 layers = {k: net[k] for k in layers}
 
 
@@ -188,16 +182,16 @@ layers = {k: net[k] for k in layers}
 input_im_theano = T.tensor4()
 outputs = lasagne.layers.get_output(layers.values(), input_im_theano)
 
-photo_features = {k: theano.shared(output.eval({input_im_theano: photo}), borrow=True)
+photo_features = {k: theano.shared(output.eval({input_im_theano: photo}))
                   for k, output in zip(layers.keys(), outputs)}
-art_features = {k: theano.shared(output.eval({input_im_theano: art}), borrow=True)
+art_features = {k: theano.shared(output.eval({input_im_theano: art}))
                 for k, output in zip(layers.keys(), outputs)}
 
 
 # In[11]:
 
 # Get expressions for layer activations for generated image
-generated_image = theano.shared(floatX(np.random.uniform(-128, 128, (1, 3, IMAGE_W, IMAGE_W))), borrow=True)
+generated_image = theano.shared(floatX(np.random.uniform(-128, 128, (1, 3, IMAGE_W, IMAGE_W))))
 
 gen_features = lasagne.layers.get_output(layers.values(), generated_image)
 gen_features = {k: v for k, v in zip(layers.keys(), gen_features)}
@@ -209,17 +203,17 @@ gen_features = {k: v for k, v in zip(layers.keys(), gen_features)}
 losses = []
 
 # content loss
-losses.append(0.001 * content_loss(photo_features, gen_features, 'conv4_2'))
+losses.append(loss_alpha1 * content_loss(photo_features, gen_features, 'conv4_2'))
 
 # style loss
-losses.append(0.2e6 * style_loss(art_features, gen_features, 'conv1_1'))
-losses.append(0.2e6 * style_loss(art_features, gen_features, 'conv2_1'))
-losses.append(0.2e6 * style_loss(art_features, gen_features, 'conv3_1'))
-losses.append(0.2e6 * style_loss(art_features, gen_features, 'conv4_1'))
-losses.append(0.2e6 * style_loss(art_features, gen_features, 'conv5_1'))
+losses.append(loss_alpha2 * style_loss(art_features, gen_features, 'conv1_1'))
+losses.append(loss_alpha2 * style_loss(art_features, gen_features, 'conv2_1'))
+losses.append(loss_alpha2 * style_loss(art_features, gen_features, 'conv3_1'))
+losses.append(loss_alpha2 * style_loss(art_features, gen_features, 'conv4_1'))
+losses.append(loss_alpha2 * style_loss(art_features, gen_features, 'conv5_1'))
 
 # total variation penalty
-losses.append(0.1e-7 * total_variation_loss(generated_image))
+losses.append(loss_alpha3 * total_variation_loss(generated_image))
 
 total_loss = sum(losses)
 
