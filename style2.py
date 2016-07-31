a8cfc1be549396e59818d92f7ab613391e7dc0dc
tkz <tkz>
2016-07-31 09:24:17 +0800

    weekends images

--- style2.py
+++ style2.py
@@ -39,9 +39,10 @@ from lasagne.nonlinearities import softmax
 
 IMAGE_W = 128
 
-loss_alpha1 = 0.001
-loss_alpha2 = 0.2
+loss_alpha1 = 1e-3
+loss_alpha2 = 0.2e4
 loss_alpha3 = 0.1e-7
+alpha = 0.8
 
 #DST='1920px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'
 #SRC='Tuebingen_Neckarfront.jpg'
@@ -52,26 +53,26 @@ DST = 'dst.jpg'
 def build_model():
     net = {}
     net['input'] = InputLayer((1, 3, IMAGE_W, IMAGE_W))
-    net['conv1_1'] = ConvLayer(net['input'], 64, 3, pad=1, flip_filters=False)
-    net['conv1_2'] = ConvLayer(net['conv1_1'], 64, 3, pad=1, flip_filters=False)
+    net['conv1_1'] = ConvLayer(net['input'], 64, 3, pad=1)
+    net['conv1_2'] = ConvLayer(net['conv1_1'], 64, 3, pad=1)
     net['pool1'] = PoolLayer(net['conv1_2'], 2, mode='average_exc_pad')
-    net['conv2_1'] = ConvLayer(net['pool1'], 128, 3, pad=1, flip_filters=False)
-    net['conv2_2'] = ConvLayer(net['conv2_1'], 128, 3, pad=1, flip_filters=False)
+    net['conv2_1'] = ConvLayer(net['pool1'], 128, 3, pad=1)
+    net['conv2_2'] = ConvLayer(net['conv2_1'], 128, 3, pad=1)
     net['pool2'] = PoolLayer(net['conv2_2'], 2, mode='average_exc_pad')
-    net['conv3_1'] = ConvLayer(net['pool2'], 256, 3, pad=1, flip_filters=False)
-    net['conv3_2'] = ConvLayer(net['conv3_1'], 256, 3, pad=1, flip_filters=False)
-    net['conv3_3'] = ConvLayer(net['conv3_2'], 256, 3, pad=1, flip_filters=False)
-    net['conv3_4'] = ConvLayer(net['conv3_3'], 256, 3, pad=1, flip_filters=False)
+    net['conv3_1'] = ConvLayer(net['pool2'], 256, 3, pad=1)
+    net['conv3_2'] = ConvLayer(net['conv3_1'], 256, 3, pad=1)
+    net['conv3_3'] = ConvLayer(net['conv3_2'], 256, 3, pad=1)
+    net['conv3_4'] = ConvLayer(net['conv3_3'], 256, 3, pad=1)
     net['pool3'] = PoolLayer(net['conv3_4'], 2, mode='average_exc_pad')
-    net['conv4_1'] = ConvLayer(net['pool3'], 512, 3, pad=1, flip_filters=False)
-    net['conv4_2'] = ConvLayer(net['conv4_1'], 512, 3, pad=1, flip_filters=False)
-    net['conv4_3'] = ConvLayer(net['conv4_2'], 512, 3, pad=1, flip_filters=False)
-    net['conv4_4'] = ConvLayer(net['conv4_3'], 512, 3, pad=1, flip_filters=False)
+    net['conv4_1'] = ConvLayer(net['pool3'], 512, 3, pad=1)
+    net['conv4_2'] = ConvLayer(net['conv4_1'], 512, 3, pad=1)
+    net['conv4_3'] = ConvLayer(net['conv4_2'], 512, 3, pad=1)
+    net['conv4_4'] = ConvLayer(net['conv4_3'], 512, 3, pad=1)
     net['pool4'] = PoolLayer(net['conv4_4'], 2, mode='average_exc_pad')
-    net['conv5_1'] = ConvLayer(net['pool4'], 512, 3, pad=1, flip_filters=False)
-    net['conv5_2'] = ConvLayer(net['conv5_1'], 512, 3, pad=1, flip_filters=False)
-    net['conv5_3'] = ConvLayer(net['conv5_2'], 512, 3, pad=1, flip_filters=False)
-    net['conv5_4'] = ConvLayer(net['conv5_3'], 512, 3, pad=1, flip_filters=False)
+    net['conv5_1'] = ConvLayer(net['pool4'], 512, 3, pad=1)
+    net['conv5_2'] = ConvLayer(net['conv5_1'], 512, 3, pad=1)
+    net['conv5_3'] = ConvLayer(net['conv5_2'], 512, 3, pad=1)
+    net['conv5_4'] = ConvLayer(net['conv5_3'], 512, 3, pad=1)
     net['pool5'] = PoolLayer(net['conv5_4'], 2, mode='average_exc_pad')
 
     return net
@@ -245,7 +246,11 @@ def eval_grad(x0):
 # In[15]:
 
 # Initialize with a noise image
-generated_image.set_value(floatX(np.random.uniform(-128, 128, (1, 3, IMAGE_W, IMAGE_W))))
+#generated_image.set_value(floatX(np.random.uniform(-128, 128, (1, 3, IMAGE_W, IMAGE_W))))
+noise = np.random.uniform(-128, 128, (1, 3, IMAGE_W, IMAGE_W))
+init_image = photo
+init_image = photo*alpha + init_image*(1-alpha)
+generated_image.set_value(floatX(init_image))
 
 x0 = generated_image.get_value().astype('float64')
 xs = []
